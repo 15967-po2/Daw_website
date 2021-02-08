a07ca57ec1589e8bf81d1151dd28b0b8a4731356
15967-po2 <65965632+15967-po2@users.noreply.github.com>
2021-02-08 01:43:07 +0000

    1st
    go

---
+++ WebSiteAPI/WebSiteAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebSiteAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
